0e86d5f014b1d3ef1125e68a1dba3975e8e76007
Jason Fehr <jasonmfehr@gmail.com>
2012-03-11 14:18:53 -0400

    Initial version of homework 2 part 2

--- Assignment2/part2.rb
+++ Assignment2/part2.rb
@@ -1,6 +1,21 @@
 class CartesianProduct
   include Enumerable
-  # your code here
+  
+  def initialize(e1, e2)
+    @e1 = e1
+    @e2 = e2
+  end
+  
+  def each
+    @e1.each {
+      |item1|
+      @e2.each {
+        |item2|
+        yield([item1, item2])
+      }
+    }
+  end
+  
 end
 
 c = CartesianProduct.new([:a,:b], [4,5])
@@ -9,7 +24,10 @@ c.each { |elt| puts elt.inspect }
 # [:a, 5]
 # [:b, 4]
 # [:b, 5]
-
+  
+c = CartesianProduct.new([:a,:b,:c], [4,5])
+c.each { |elt| puts elt.inspect }
+  
 c = CartesianProduct.new([:a,:b], [])
 c.each { |elt| puts elt.inspect }
 # (nothing printed since Cartesian product
